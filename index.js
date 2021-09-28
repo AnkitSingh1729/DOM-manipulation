60208a3c3a0ff3fb6d7e3dfc3c7bcf0004002082
AnkitSingh1729 <64279281+AnkitSingh1729@users.noreply.github.com>
2021-09-28 12:25:20 +0530

    header color update

--- index.js
+++ index.js
@@ -30,17 +30,26 @@ function onSubmit(e) {
  
 }
 
-console.dir(document);
-console.log(document.domain);
-console.log(document.URL);
-console.log(document.title);
-document.title = "New Title";
-console.log(document.doctype);
-console.log(document.head);
-console.log(document.body);
-console.log(document.all);
-console.log(document.all[10]);
-// document.all[10].textContent = 'Hello';
-console.log(document.forms[0]);
-console.log(document.links);
-console.log(document.images);
+// console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// document.title = "New Title";
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// // document.all[10].textContent = 'Hello';
+// console.log(document.forms[0]);
+// console.log(document.links);
+// console.log(document.images);
+
+var headerTitle = document.getElementById('header-title');
+// console.log(headerTitle.innerText);
+console.log(headerTitle.textContent);
+
+// headerTitle.innerHTML = '<h1>Hello</h1>';
+headerTitle.style.backgroundColor = 'grey';
+headerTitle.style.color = 'green';
+headerTitle.style.borderBottom = '3px solid #000';
