61d5183e7b9161c8c6ad9178e579ab8d23dc2986
AnkitSingh1729 <64279281+AnkitSingh1729@users.noreply.github.com>
2021-09-28 16:04:15 +0530

    DOM traversal

--- index.js
+++ index.js
@@ -46,35 +46,35 @@ function onSubmit(e) {
 // console.log(document.images);
 
 
-// Lesson : getElementbyId
-var headerTitle = document.getElementById('header-title');
-// console.log(headerTitle.innerText);
-console.log(headerTitle.textContent);
+// // Lesson : getElementbyId
+// var headerTitle = document.getElementById('header-title');
+// // console.log(headerTitle.innerText);
+// console.log(headerTitle.textContent);
 
-// headerTitle.innerHTML = '<h1>Hello</h1>';
-headerTitle.style.backgroundColor = 'grey';
-headerTitle.style.color = 'green';
-headerTitle.style.borderBottom = '3px solid #000';
-
-
-// Lesson : getElementbyclassName
-var item = document.getElementsByClassName('item');
-console.log(item);
-item[2].textContent = 'Hello 2';
-item[1].style.backgroundColor = 'green';
-for (var i = 0; i<item.length; i++){
-    item[i].style.fontWeight = 'bold';
-}
+// // headerTitle.innerHTML = '<h1>Hello</h1>';
+// headerTitle.style.backgroundColor = 'grey';
+// headerTitle.style.color = 'green';
+// headerTitle.style.borderBottom = '3px solid #000';
 
 
-// Lesson : getElementbyTagName
-var itemli = document.getElementsByTagName('li');
-console.log(itemli);
-itemli[2].textContent = 'Item 2';
-itemli[1].style.backgroundColor = 'green';
-for (var i = 0; i<itemli.length; i++){
-    itemli[i].style.fontWeight = 'bold';
-}
+// // Lesson : getElementbyclassName
+// var item = document.getElementsByClassName('item');
+// console.log(item);
+// item[2].textContent = 'Hello 2';
+// item[1].style.backgroundColor = 'green';
+// for (var i = 0; i<item.length; i++){
+//     item[i].style.fontWeight = 'bold';
+// }
+
+
+// // Lesson : getElementbyTagName
+// var itemli = document.getElementsByTagName('li');
+// console.log(itemli);
+// itemli[2].textContent = 'Item 2';
+// itemli[1].style.backgroundColor = 'green';
+// for (var i = 0; i<itemli.length; i++){
+//     itemli[i].style.fontWeight = 'bold';
+// }
 
 
 // Lesson : query Selector
@@ -82,19 +82,19 @@ var header = document.querySelector('#header-title');
 header.style.borderBottom = '4px solid #ccc';
 // itemq[1].style.backgroundColor = 'green';
 
-var input = document.querySelector('input');
-input.value = 'Hello World';
+// var input = document.querySelector('input');
+// input.value = 'Hello World';
 
-var submit = document.querySelector('input[type="submit"]');
-submit.value = "SEND";
+// var submit = document.querySelector('input[type="submit"]');
+// submit.value = "SEND";
 
-var item = document.querySelector('.item');
-item.style.color = 'red';
+// var item = document.querySelector('.item');
+// item.style.color = 'red';
 
-var lastitem = document.querySelector('.item:nth-child(2)');
-lastitem.style.backgroundColor = 'green';
-var invisibleitem = document.getElementsByClassName('item');
-invisibleitem[2].style.visibility = 'hidden';
+// var lastitem = document.querySelector('.item:nth-child(2)');
+// lastitem.style.backgroundColor = 'green';
+// var invisibleitem = document.getElementsByClassName('item');
+// invisibleitem[2].style.visibility = 'hidden';
 
 
 
@@ -110,3 +110,67 @@ for(var i=0; i<odd.length; i++){
     odd[i].style.backgroundColor = "#f4f4f4";
     even[i].style.backgroundColor = "#ccc";
 }
+
+
+
+// Traversing the dom
+var itemlist = document.querySelector('.items');
+// console.log(itemlist);
+// console.log(itemlist.parentNode);
+// itemlist.parentNode.style.color = "#f3f3f3"
+
+
+// childNodes
+// console.log(itemlist.childNodes);   // it includes line break
+// console.log(itemlist.children);       // ignores line break and just give actual children
+// itemlist.children[1].style.backgroundColor = 'green';
+// console.log(itemlist.children[1]);
+
+
+// // First child
+// console.log(itemlist.firstChild);
+// // First element child
+// console.log(itemlist.firstElementChild);
+// itemlist.firstElementChild.textContent = 'Hello 1';
+
+// // last element child
+// console.log(itemlist.lastChild);
+// console.log(itemlist.lastElementChild);
+
+// // next sibling
+// console.log(itemlist.nextSibling);
+// console.log(itemlist.nextElementSibling);
+
+
+// // previous sibling
+// console.log(itemlist.previousSibling);
+// console.log(itemlist.previousElementSibling);
+
+
+// create Element
+// create a div
+var newDiv = document.createElement('div');
+
+// Add class
+newDiv.className = 'hello';
+
+// Add id
+newDiv.id = 'hello1';
+
+// Add attr
+newDiv.setAttribute('tittle', 'Hello Div');
+
+// Create text node
+var newDivText = document.createTextNode('Hello World');
+
+// Add text to div
+newDiv.appendChild(newDivText);
+
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+
+container.insertBefore(newDiv, h1);
+
+console.log(h1);
+console.log(container);
+console.log(newDiv);
