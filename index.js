5c984dd6e999c9340f30fda63278c3f6786f91f0
AnkitSingh1729 <64279281+AnkitSingh1729@users.noreply.github.com>
2021-09-28 11:48:46 +0530

    document console log

--- index.js
+++ index.js
@@ -29,3 +29,18 @@ function onSubmit(e) {
     }
  
 }
+
+console.dir(document);
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.title);
+document.title = "New Title";
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
+console.log(document.all);
+console.log(document.all[10]);
+// document.all[10].textContent = 'Hello';
+console.log(document.forms[0]);
+console.log(document.links);
+console.log(document.images);
